ca8ababdc7db44faf09190234545c870fc8922a1
KirillDolgopolov <Kirdolgopolov.it@gmail.com>
2023-11-06 10:38:14 +0100

    first commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/dolgopolov/kirill/s05/t02/S05T02DolgopolovKirillApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.dolgopolov.kirill.s05.t02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T02DolgopolovKirillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T02DolgopolovKirillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/dolgopolov/kirill/s05/t02/S05T02DolgopolovKirillApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.dolgopolov.kirill.s05.t02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T02DolgopolovKirillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
